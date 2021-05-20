1334f497420907cd2cd4c237d3d71826fe70ca13
Eva Belinskaya <evabelinskaa336@gmail.com>
2021-05-20 17:44:36 +0300

    Update cards on Landing page

--- calminder/src/services/firebase.service.js
+++ calminder/src/services/firebase.service.js
@@ -1,2 +1,21 @@
 import firebase from '../Firebase';
+export const getFaqs = () => {
+    return new Promise((resolve, reject) => {
+        firebase.database().ref('questions').on('value', snapshot => {
+            let array = [];
+            snapshot.forEach(snap => array.push(snap.val()))
+            return resolve(array);
+        });
+    })
+}
 
+
+export const getCards = () => {
+    return new Promise((resolve, reject) => {
+        firebase.database().ref('Calminder-exercise').on('value', snapshot => {
+            let array = [];
+            snapshot.forEach(snap => array.push(snap.val()))
+            return resolve(array);
+        });
+    })
+}
