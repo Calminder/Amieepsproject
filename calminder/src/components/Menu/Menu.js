c5988350294374b6aafc1f7b6bb841f4913fd4c4
AyoubAouraghe <45266255+AyoubAouraghe@users.noreply.github.com>
2021-05-24 15:43:36 +0200

    menu uncoment

--- calminder/src/components/Menu/Menu.js
+++ calminder/src/components/Menu/Menu.js
@@ -1,39 +1,40 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import styles from './Menu.module.css';
 import { Link } from './Link.js';
-export const Menu =() => {
+export const Menu = () =>
+{
     const items = [
         {
-            value:'Home',
-            href:'/',
+            value: 'Home',
+            href: '/',
         },
         {
-            value:'About',
-            href:'/about',
+            value: 'About',
+            href: '/about',
         },
         {
-            value:'FAQ',
-            href:'/faq',
+            value: 'FAQ',
+            href: '/faq',
         }
     ];
-    const [open, setOpen]= useState (true);
+    const [open, setOpen] = useState(true);
 
-    return(
-        // <div className={styles.container}>
-        //     <div className={styles.closeIcon} >X</div>
-        //     <div className={styles.menu}>
-        //         <nav>
-        //             <div className={styles.burgerbtn} onClick={() => setOpen((currentOpen) => currentOpen = !currentOpen)}>
-        //                 <span></span>
-        //             </div>
-        //         </nav>
+    return (
+        <div className={styles.container}>
+            <div className={styles.closeIcon} >X</div>
+            <div className={styles.menu}>
+                <nav>
+                    <div className={styles.burgerbtn} onClick={() => setOpen((currentOpen) => currentOpen = !currentOpen)}>
+                        <span></span>
+                    </div>
+                </nav>
 
-        //         <div className={styles.menu_content} style={{ display:  open ? 'block' : 'none' }}>
-        //             <Link items={items} />
-        //         </div>
-        //     </div>
-        // </div>
-        <div></div>
+                <div className={styles.menu_content} style={{ display: open ? 'block' : 'none' }}>
+                    <Link items={items} />
+                </div>
+            </div>
+        </div>
+
 
     )
 };
