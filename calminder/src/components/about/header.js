10975bfedb46a13342a198ec38433eda68434184
AyoubAouraghe <45266255+AyoubAouraghe@users.noreply.github.com>
2021-06-04 17:23:50 +0200

    about page logo + fixed menu button position fixed
    line 31 changed it from relative to fixed

---
+++ calminder/src/components/about/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from "./about.module.css";
+function Header()
+{
+    return (
+        <>
+            <header>
+                <div className={styles.logo}></div>
+
+            </header>
+
+        </>
+
+
+    )
+}
+
+export default Header;
