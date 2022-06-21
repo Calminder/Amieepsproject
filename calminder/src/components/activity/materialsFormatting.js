31aaa64b318558e484c28c3cc7abd5c18166c28b
Calminder <calminderap@gmail.com>
2022-06-21 15:46:35 +0200

    deleted unnecessary If statement

--- calminder/src/components/activity/materialsFormatting.js
+++ calminder/src/components/activity/materialsFormatting.js
@@ -4,20 +4,17 @@ const materialsFormatting = (text) => {
     let solidString = "";
     let buf = text;
     typeof buf == "string" ? solidString = buf.trim() : solidString = "";
-    if (solidString.length > 0)
-    {
-        while (solidString.length > 0 ) {
-            let position = solidString.length - 1;
-            do {
-                position--;
-            }
-            while (((solidString.charCodeAt(position) != 45 || solidString.charCodeAt(position+1) != 32) || (solidString.charCodeAt(position+1) != 32)) && (position > 0));
-            let end = solidString.length;
-            let p = solidString.substring(position, end);
-            paragraphs.push(p);
-            let buf = solidString.slice(0, position);
-            solidString = buf.trim();
+    while (solidString.length > 0 ) {
+        let position = solidString.length - 1;
+        do {
+            position--;
         }
+        while (((solidString.charCodeAt(position) != 45 || solidString.charCodeAt(position+1) != 32) || (solidString.charCodeAt(position+1) != 32)) && (position > 0));
+        let end = solidString.length;
+        let p = solidString.substring(position, end);
+        paragraphs.push(p);
+        let buf = solidString.slice(0, position);
+        solidString = buf.trim();
     }
     paragraphs.reverse();
     return paragraphs;
