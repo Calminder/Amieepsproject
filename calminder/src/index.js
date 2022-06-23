c7d7eb8ac1308b05b70f0e535a2e1b58d01d4bf0
Yurii-30 <81021827+Yurii-30@users.noreply.github.com>
2022-06-23 18:53:56 +0200

    Delete calminder directory

--- calminder/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles.css'
-import {AnimationLandingPage} from './components/AnimationLandingPage'
-
-ReactDOM.render(
-  <AnimationLandingPage />, 
-  document.getElementById('root')
-);
--- calminder/src/services/card.service.js
+++
@@ -1,4 +0,0 @@
-import { categories } from '../constants/cards';
-
-export const getCardImageByCategory = (category) => categories.find(c => c.name === category);
-// c  - returns a pair: [image, category]
