ddb3ad33ff00dcf22ec315e3a3f95ce81ed1101b
Yurii Herasin <yuriu.gerasin@gmail.com>
2023-10-13 15:44:39 +0200

    Social Media icons

--- calminder/src/cards/header.js
+++ calminder/src/cards/header.js
@@ -6,6 +6,27 @@ function Header()
         <header>
             <div className={styles.logo}></div>
             <div className={styles.amielogo}></div>
+            <div className={styles.social_media_links_first}>
+                <a href = "https://www.youtube.com/@ArtsandMindfulnessinEducation" 
+                target = "_blank">
+                    <div className={styles.youtube}></div> 
+                </a>
+                <a href = "https://www.instagram.com/artsandmindfulness/"
+                target = "_blank">
+                    <div className={styles.instagram}></div>
+                </a>
+            </div>
+            <div className={styles.erasmuslogo}></div>
+            <div className={styles.social_media_links_last}>
+                <a href = "https://www.facebook.com/groups/www.speleninstilte.nl/"
+                target = "_blank">
+                    <div className={styles.facebook}></div> 
+                </a>
+                <a href = "https://www.irmasmegen.com/en/"
+                target = "_blank">
+                    <div className={styles.website}></div>
+                </a>
+            </div>
         </header>
 
 
