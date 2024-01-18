6b676ecf9c0c0cea101e71f5ec441f43f646b12e
Yurii Herasin <yuriu.gerasin@gmail.com>
2024-01-18 12:45:10 +0100

    framework change

--- calminder/src/components/framework/framework.js
+++ calminder/src/components/framework/framework.js
@@ -3,18 +3,8 @@ import styles from './framework.module.scss';
 
 import Header from './header'
 
-
 export const Framework = () =>
 {
-    const onButtonClick = () => {
-        const pdfUrl = "pedagogical framework_AMiE.pdf";
-        const link = document.createElement("a");
-        link.href = pdfUrl;
-        link.download = pdfUrl;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
 
     return (
         <div>
@@ -28,11 +18,8 @@ export const Framework = () =>
                     <div className = {styles.full}>
                         <p>Hello there!</p>
                         <p>To help you bring arts and mindfulness in education we have designed a simple pedagogical framework.</p>
+                        <p>You can find the AMiE pedagogical framework via the following link: <a href = "https://drive.google.com/file/d/1kJGF5icOZkMZHrcRpDdckz5XhHr9xsB7/view?usp=sharing" target = "_blank" >A.M.i.E. Pedagogical Framework</a></p>
                         <p>If you want to know more about how you can use this framework make sure to follow the free AMiE online course (available on Udemy)</p>
-                        <p>You can find the AMiE pedagogical framework here:</p>
-                        <button onClick = {onButtonClick}>
-                            Download PDF
-                        </button>
                     </div>
                 </section>
             </div>
